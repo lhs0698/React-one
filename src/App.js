d17227430dc27930df12653328c1257c720e64c9
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-07-11 22:01:13 +0900

    input box

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form'
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
 
 function App() {
-
   return (
     <div className="App">
       <Top></Top>
+      <LoginInput></LoginInput>
     </div>
   );
 }
 
+// navbar
 function Top() {
   return (
     <Navbar bg="dark" variant="dark">
@@ -26,6 +29,24 @@ function Top() {
         </Container>
       </Navbar>
   )
+}
+
+// login-input-box
+function LoginInput() {
+  return (
+    <>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="ID"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+    </>
+  );
 }
 
 export default App;
