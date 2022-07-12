16241690e45df60e9c0e84a09a6c75a56f4b3200
lhs0698 <104623873+lhs0698@users.noreply.github.com>
2022-07-12 16:29:35 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form'
 import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
+import Button from 'react-bootstrap/Button';
 
 function App() {
+
+
   return (
     <div className="App">
       <Top></Top>
       <LoginInput></LoginInput>
+      <Loginbtn></Loginbtn>
     </div>
   );
 }
@@ -18,35 +22,52 @@ function App() {
 // navbar
 function Top() {
   return (
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">React</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">로그인</Nav.Link>
+          <Nav.Link href="#features">회원가입</Nav.Link>
+          <Nav.Link href="#pricing">기타</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
 // login-input-box
 function LoginInput() {
   return (
-    <>
+    <div className='inputWrap'>
       <FloatingLabel
         controlId="floatingInput"
         label="ID"
-        className="mb-3"
+        className="mb-2"
       >
         <Form.Control type="email" placeholder="name@example.com" />
       </FloatingLabel>
       <FloatingLabel controlId="floatingPassword" label="Password">
         <Form.Control type="password" placeholder="Password" />
       </FloatingLabel>
-    </>
+    </div>
   );
 }
+
+// 로그인, 회원가입 Button
+function Loginbtn() {
+  function Click() {
+    console.log("click");
+  }
+  return (
+    <div className="d-grid gap-2" id='Login'>
+      <Button variant="primary" size="lg" onClick={Click}>
+        로그인
+      </Button>
+      <Button variant="danger" size="lg">
+        회원가입
+      </Button>
+    </div>
+  )
+};
 
 export default App;
